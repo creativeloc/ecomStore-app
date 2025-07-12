b3baa735c63237ee619803769c7a1075d5ba2ed1
Lawrence Caulker <locaulker@gmail.com>
2025-07-12 08:06:30 -0400

    setup and implement light and dark mode in the navbar; use boolean to switch between modes

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button"
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-3xl">HomePage</h1>
-      <Button variant="destructive" size="lg" className="uppercase m-8">
+      <h1 className="text-3xl text-muted-foreground">HomePage</h1>
+      <Button variant="outline" size="lg" className="uppercase m-8">
         Click Me
       </Button>
     </div>

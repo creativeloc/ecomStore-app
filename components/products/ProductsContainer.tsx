456052b802619d1d4c2d11af31e4a1d15acab5c2
creativeloc <creativeloc@yahoo.com>
2025-07-19 21:10:27 -0400

    complete grid layout functionality

--- components/products/ProductsContainer.tsx
+++ components/products/ProductsContainer.tsx
@@ -16,12 +16,40 @@ const ProductsContainer = async ({
   const products = await fetchAllProducts()
   const totalProducts = products.length
 
-  const searchTerms = search ? `&search=${search}` : ""
+  const searchTerm = search ? `&search=${search}` : ""
 
   return (
     <>
       {/* HEADER */}
-      <section></section>
+      <section>
+        <div className="flex justify-between items-center">
+          <h4 className="font-medium text-lg">
+            {totalProducts} product{totalProducts > 1 && "s"}
+          </h4>
+          <div className="flex gap-x-4">
+            <Button
+              variant={layout === "grid" ? "default" : "ghost"}
+              size="icon"
+              asChild
+            >
+              <Link href={`/products?layout=grid${searchTerm}`}>
+                <LuLayoutGrid />
+              </Link>
+            </Button>
+
+            <Button
+              variant={layout === "list" ? "default" : "ghost"}
+              size="icon"
+              asChild
+            >
+              <Link href={`/products?layout=list${searchTerm}`}>
+                <LuList />
+              </Link>
+            </Button>
+          </div>
+        </div>
+        <Separator className="mt-4" />
+      </section>
       {/* PRODUCTS */}
       <div>
         {totalProducts === 0 ? (

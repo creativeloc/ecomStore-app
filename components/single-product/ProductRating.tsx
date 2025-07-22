e3fb098ad807b64668e533d01d4e254328ba49ca
creativeloc <creativeloc@yahoo.com>
2025-07-22 02:22:54 -0400

    construct single product details boiler plate

---
+++ components/single-product/ProductRating.tsx
@@ -0,0 +1,20 @@
+import { FaStar } from "react-icons/fa"
+
+const ProductRating = ({ productId }: { productId: string }) => {
+  // temp
+  const rating = 4.2
+
+  // nom reviews
+  const count = 25
+
+  const className = `flex gap-1 items-center text-md mt-1 mb-4`
+  const countValue = `(${count}) reviews`
+
+  return (
+    <span className={className}>
+      <FaStar className="w-3 h-3" />
+      {rating} {countValue}
+    </span>
+  )
+}
+export default ProductRating

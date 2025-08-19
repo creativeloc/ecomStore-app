10199b5d9fc1771ee64ca52381e2756a465c7ebe
creativeloc <creativeloc@yahoo.com>
2025-08-19 04:13:16 -0400

    add checbox input form

--- components/form/TextAreaInput.tsx
+++ components/form/TextAreaInput.tsx
@@ -1,6 +1,5 @@
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
-import TextAreaInput from "./TextAreaInput"
 
 type TextAreaInputProps = {
   name: string

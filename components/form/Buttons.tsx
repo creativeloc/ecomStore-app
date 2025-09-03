3fc62f819b3d4e6eecadc0393171dbdb1857fe21
creativeloc <creativeloc@yahoo.com>
2025-09-03 00:48:31 -0400

    add icon buutons and delete product action

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -5,8 +5,8 @@ import { useFormStatus } from "react-dom"
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { SignInButton } from "@clerk/nextjs"
-import { FaRegHeart, FaHeart } from "react-icons/fa"
-import { LuTrash2, LuSquare } from "react-icons/lu"
+import { FaRegHeart, FaHeart, FaRegEdit } from "react-icons/fa"
+import { LuTrash2 } from "react-icons/lu"
 
 type btnSize = "default" | "lg" | "sm"
 
@@ -41,3 +41,32 @@ const SubmitButton = ({
   )
 }
 export default SubmitButton
+
+type actionType = "edit" | "delete"
+
+export const IconButton = ({ actionType }: { actionType: actionType }) => {
+  const { pending } = useFormStatus()
+
+  const renderIcon = () => {
+    switch (actionType) {
+      case "edit":
+        return <FaRegEdit />
+      case "delete":
+        return <LuTrash2 />
+      default:
+        const never: never = actionType
+        throw new Error(`Invalid action type: ${never}`)
+    }
+  }
+
+  return (
+    <Button
+      type="submit"
+      size="icon"
+      variant="link"
+      className="p-2 cursor-pointer"
+    >
+      {pending ? <ReloadIcon className="animate-spin" /> : renderIcon()}
+    </Button>
+  )
+}
